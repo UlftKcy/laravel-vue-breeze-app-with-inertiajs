d2d89ce42d756fc8e28bc24a9ae9250e3bbe614c
ulfet <ukacay@albasoft.com.tr>
2022-07-07 15:32:29 +0300

    added role and edited routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-const path = require('path');
 
 export default defineConfig({
     plugins: [
@@ -18,9 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    resolve: {
-        alias: {
-            ziggy: path.resolve('vendor/tightenco/ziggy/src/js/route.js'),
-        },
-    },
 });
