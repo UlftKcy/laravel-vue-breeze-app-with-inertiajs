4e11c9e532a925cb341c5b4aee92d775a6e3c6fd
Ülfet <ukacay87@gmail.com>
2022-07-16 22:52:27 +0300

    vite html lib added

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+const path = require('path');
 
 export default defineConfig({
     resolve: {
@@ -7,4 +8,23 @@ export default defineConfig({
             'ziggy-vue': '/vendor/tightenco/ziggy/src/js/vue',
         },
     },
+    build: {
+        lib: {
+          entry: path.resolve(__dirname, 'lib/main.js'),
+          name: 'MyLib',
+          fileName: (format) => `my-lib.${format}.js`
+        },
+        rollupOptions: {
+          // make sure to externalize deps that shouldn't be bundled
+          // into your library
+          external: ['vue'],
+          output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              vue: 'Vue'
+            }
+          }
+        }
+      }
 });
